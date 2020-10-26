61901d977ee31172f7b04fc1dd1b5d6a83489bf5
3301085375@qq.com <3301085375@qq.com>
2020-10-26 23:13:31 +0800

    彩色图像通道的分离与合并

--- Python_Code/Image Processing_JZG/色彩空间_02.py
+++ Python_Code/Image Processing_JZG/色彩空间_02.py
@@ -8,6 +8,9 @@ def split_img(image):
     cv.imshow('blue', b)
     cv.imshow('green', g)
     cv.imshow('red', r)
+    cv.imwrite('out/split_img/blue.jpg', b)
+    cv.imwrite('out/split_img/green.jpg', g)
+    cv.imwrite('out/split_img/red.jpg', r)
     return b, g, r
 
 
@@ -15,10 +18,12 @@ def split_img(image):
 def merge_img(b, g, r):
     image = cv.merge([b, g, r])
     cv.imshow('merge', image)
+    cv.imwrite('out/merge_img/merge.jpg', image)
 
     # 所有像素 第三通道赋0值
     image[:, :, 2] = 0
     cv.imshow('red = 0', image)
+    cv.imwrite('out/merge_img/merge_without_red.jpg', image)
 
 
 def main():
