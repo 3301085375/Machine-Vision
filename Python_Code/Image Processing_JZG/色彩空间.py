4f5d27eafae970717ad399ad2a3a8558394d9422
3301085375@qq.com <3301085375@qq.com>
2020-10-27 21:37:35 +0800

    几种色彩空间的练习

--- Python_Code/Image Processing_JZG/色彩空间.py
+++ Python_Code/Image Processing_JZG/色彩空间.py
@@ -5,14 +5,19 @@ import cv2 as cv
 def color_space_demo(image):
     gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
     cv.imshow('GRAY', gray)
+    cv.imwrite('out/color_space/GRAY.jpg', gray)
     hsv = cv.cvtColor(image, cv.COLOR_BGR2HSV)
     cv.imshow('HSV', hsv)
+    cv.imwrite('out/color_space/HSV.jpg', hsv)
     hls = cv.cvtColor(image, cv.COLOR_BGR2HLS)
     cv.imshow('HIS', hls)
+    cv.imwrite('out/color_space/HIS.jpg', hls)
     YCrCb = cv.cvtColor(image, cv.COLOR_BGR2YCrCb)
     cv.imshow('YCrCb', YCrCb)
+    cv.imwrite('out/color_space/YCrCb.jpg', YCrCb)
     yuv = cv.cvtColor(image, cv.COLOR_BGR2YUV)
     cv.imshow('YUV', yuv)
+    cv.imwrite('out/color_space/YUV.jpg', yuv)
 
 
 img = cv.imread('sources/butterfly.jpg')
